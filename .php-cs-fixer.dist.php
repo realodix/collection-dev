664113bacd0808d7907c7b442daf07ea52856ebc
Kei <budi03@gmail.com>
2024-07-11 18:09:31 +0700

    realodix/relax v1.15

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,12 +1,11 @@
 <?php
 
 use Realodix\Relax\Config;
-use Realodix\Relax\RuleSet\Sets\Realodix;
 
 $localRules = [
     // ...
 ];
 
-return Config::create(new Realodix)
+return Config::create('relax')
     ->setRules($localRules)
     ->setCacheFile(__DIR__.'/.tmp/.php-cs-fixer.cache');
